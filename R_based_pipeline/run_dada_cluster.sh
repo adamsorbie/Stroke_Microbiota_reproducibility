8d35e8bc8e73e147e88390156bb458a6e2efc54a
Adam Sorbie <adam.sorbie@med.uni-muenchen.de>
2023-02-01 13:49:25 +0100

    Update run_dada_cluster.sh
    skeleton of slurm-based batch script

--- R_based_pipeline/run_dada_cluster.sh
+++ R_based_pipeline/run_dada_cluster.sh
@@ -1,1 +1,34 @@
-Rscript run_dada2_SE.R -p ~/data/meta-analysis_human/Gu_et_al-2021-CH-PRJCA004950/fastq/trimmed_primer/ -o ~/data/meta-analysis_human/Gu_et_al-2021-CH-PRJCA004950/dada2_out -f 220 -o ~/data/meta-analysis_human/Gu_et_al-2021-CH-PRJCA004950/dada2_out -n 2 -t 6
+## SLURM batch job example
+
+# Adjust SBATCH commands below according to your cluster setup
+
+#SBATCH -o ./%x.%j.%N.out
+#SBATCH -D ./
+#SBATCH -J "16S_analysis"
+#SBATCH --get-user-env
+#SBATCH --clusters=cluster_name
+#SBATCH --partition=cluster_partition
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=50gb
+#SBATCH --mail-type=end
+#SBATCH --mail-user=user@email.com
+#SBATCH --export=NONE
+#SBATCH --time=08:00:00
+
+# Pipeline parameters
+FILEPATH=
+OUTPUT=
+TRIM_F=
+TRIM_R
+N_ERROR=
+THREADS=
+MODE="PE" # change to SE if working with single-end reads
+
+if [ "$MODE" == "PE" ]; then
+    Rscript run_dada2_PE.R -p $FILEPATH -o $OUTPUT -f $TRIM_F -r $TRIM_R -n $N_ERROR  -t $THREADS  
+elif [ "$MODE" == "SE" ]
+    Rscript run_dada2_SE.R -p $FILEPATH -o $OUTPUT -f $TRIM_F -n $N_ERROR  -t $THREADS
+else 
+   exit 
+fi
+
